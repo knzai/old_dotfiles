519934b440ed9fbbaf014b44f4e378b85ef16d99
Tim Connor <timocratic@gmail.com>
2010-10-03 17:35:51 -0700

    add git-wtf and cleanup gitconfig

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -4,8 +4,8 @@ REPO_RED='\[\e[01;31m\]'
 PATH_BLUE='\[\e[01;36m\]'
 END_COLOR='\[\e[m\]'
 
-#PS1="${USER_GREEN}\u${END_COLOR}${REPO_RED}\$(git_br)${END_COLOR}:${PATH_BLUE}\$(git_pwd)${END_COLOR}$ "
+PS1="${USER_GREEN}\u${END_COLOR}${REPO_RED}\$(git_br)${END_COLOR}:${PATH_BLUE}\$(git_pwd)${END_COLOR}$ "
 
 #git-prompt
-[[ $- == *i* ]]   &&   . ~/.dotfiles/git-prompt/git-prompt.sh
+#[[ $- == *i* ]]   &&   . ~/.dotfiles/git-prompt/git-prompt.sh
 
