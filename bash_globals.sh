90ac4f09c1b5d1c33969730099dee78407e0a063
Kenzi Connor <kenzi@cloudcity.io>
2019-11-21 14:58:58 -0800

    setting this this way breaks things now

--- bash_globals.sh
+++ bash_globals.sh
@@ -1,6 +1,5 @@
 export PATH=~/.dotfiles/bin/git_ps_tools:~/.dotfiles/bin:~/bin:./bin:$PATH
 export LSCOLORS=dxfxcxdxbxegedabagacad
-export RAILS_ENV=development
 export dot=~/.dotfiles
 export CDPATH=~/Projects
 export BUNDLER_EDITOR="atom"
